0c72fdec82ba5115df89637c440f3e76a0aff55d
qyd62uwgrky <sorted_viewer0c@icloud.com>
2025-11-27 10:00:00 +0800

    add hardhat config with fhevm plugin

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@fhevm/hardhat-plugin");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+      evmVersion: "cancun",
+    }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      gasPrice: "auto"
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
+  }
+};
